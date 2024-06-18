42de5f4e1230da436c9b89291533d747e5950422
leticia kanno <106741944+Leticiakanno@users.noreply.github.com>
2024-06-18 13:05:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,121 +1,135 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaAlternativas =
+document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-
 const perguntas = [
-   {
-       enunciado: "qual seria sua reação se todo o material fisico (material de estudo) fosse substituído por apostilas virtuais, qual seria sua reação?",
-       alternativas: [
-           {
-               texto: "Ieu iria amar isso!",
-               afirmacao: "afirmação"
-           },
-           {
-               texto: "Isso é terrível!",
-               afirmacao: "afirmação"
-           }
-       ]
-   },
-   {
-       enunciado: "voce faz uso do chat gpt?",
-       alternativas: [
-           {
-               texto: "sim.",
-               afirmacao: "afirmação"
-           },
-           {
-               texto: "não.",
-               afirmacao: "afirmação"
-           }
-       ]
-   },
-   {
-       enunciado: "o auxilio da inteligencia artificial pode modificar o verdadeiro significado das pesquisas feitas pelo ser humano?",
-       alternativas: [
-           {
-               texto: "sim, pois a IA nao tem embasamento cientifico.",
-               afirmacao: "afirmação"
-           },
-           {
-               texto: "nao, ela apenas mostra fontes de pesquisa.",
-               afirmacao: "afirmação"
-           }
-       ]
-   },
-   {
-       enunciado: "vc acha ético ultilizar IA na criação de trabalhos escolares?",
-       alternativas: [
-           {
-               texto: "sim, não há nada de errado nisso.",
-               afirmacao: "afirmação"
-           },
-           {
-               texto: "não, pois nós temos capacidade de fazer isso sozinhos.",
-               afirmacao: "afirmação"
-           }
-       ]
-   },
-   {
-       enunciado: "as artes geradas por IA podem ser consideradas artes renomadas? ",
-       alternativas: [
-           {
-               texto: "sim.",
-               afirmacao: "afirmação"
-           },
-           {
-               texto: "não.",
-               afirmacao: "afirmação"
-           }
-       ]
-   },
+{
+enunciado: "qual seria sua reação se todo o material fisico
+(material de estudo) fosse substituído por apostilas virtuais, qual
+seria sua reação?",
+alternativas: [
+{
+texto: "Isso é assustador!",
+afirmacao: "afirmação"
+},
+{
+texto: "Isso é maravilhoso!",
+afirmacao: "afirmação"
+}
+]
+},
+{
+enunciado: "Com a descoberta desta tecnologia, chamada
+Inteligência Artificial, uma professora de tecnologia da escola decidiu
+fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula
+ela pede que você escreva um trabalho sobre o uso de IA em sala de
+aula. Qual atitude você toma?",
+alternativas: [
+{
+texto: "Utiliza uma ferramenta de busca na internet que
+utiliza IA para que ela ajude a encontrar informações relevantes para o
+trabalho e explique numa linguagem que facilite o entendimento.",
+afirmacao: "afirmação"
+},
+{
+texto: "Escreve o trabalho com base nas conversas que
+teve com colegas, algumas pesquisas na internet e conhecimentos
+próprios sobre o tema.",
+afirmacao: "afirmação"
+}
+]
+},
+{
+enunciado: "Após a elaboração do trabalho escrito, a professora
+realizou um debate entre a turma para entender como foi realizada a
+pesquisa e escrita. Nessa conversa também foi levantado um ponto muito
+importante: como a IA impacta o trabalho do futuro. Nesse debate, como
+você se posiciona?",
+alternativas: [
+{
+texto: "Defende a ideia de que a IA pode criar novas
+oportunidades de emprego e melhorar habilidades humanas.",
+afirmacao: "afirmação"
+},
+{
+texto: "Me preocupo com as pessoas que perderão seus
+empregos para máquinas e defendem a importância de proteger os
+trabalhadores.",
+afirmacao: "afirmação"
+}
+]
+},
+{
+enunciado: "Ao final da discussão, você precisou criar uma
+imagem no computador que representasse o que pensa sobre IA. E agora?",
+alternativas: [
+{
+texto: "Criar uma imagem utilizando uma plataforma de
+design como o Paint.",
+afirmacao: "afirmação"
+},
+{
+texto: "Criar uma imagem utilizando um gerador de imagem
+de IA.",
+afirmacao: "afirmação"
+}
+]
+},
+{
+enunciado: "Você tem um trabalho em grupo de biologia para
+entregar na semana seguinte, o andamento do trabalho está um pouco
+atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O
+problema é que o trabalho está totalmente igual ao do chat. O que você
+faz? ",
+alternativas: [
+{
+texto: "Escrever comandos para o chat é uma forma de
+contribuir com o trabalho, por isso não é um problema utilizar o texto
+inteiro.",
+afirmacao: "afirmação"
+},
+{
+texto: "O chat pode ser uma tecnologia muito avançada,
+mas é preciso manter a atenção pois toda máquina erra, por isso revisar
+o trabalho e contribuir com as perspectivas pessoais é essencial.",
+afirmacao: "afirmação"
+}
+]
+},
 ];
-
-
-
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
-
-
 function mostraPergunta() {
-   if (atual >= perguntas.length) {
-       mostraResultado();
-       return;
-   }
-   perguntaAtual = perguntas[atual];
-   caixaPerguntas.textContent = perguntaAtual.enunciado;
-   caixaAlternativas.textContent = "";
-   mostraAlternativas();
+if (atual >= perguntas.length) {
+mostraResultado();
+return;
 }
-
-
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+caixaAlternativas.textContent = "";
+mostraAlternativas();
+}
 function mostraAlternativas(){
-   for(const alternativa of perguntaAtual.alternativas) {
-       const botaoAlternativas = document.createElement("button");
-       botaoAlternativas.textContent = alternativa.texto;
-       botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-       caixaAlternativas.appendChild(botaoAlternativas);
-   }
+for(const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () =>
+respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
 }
-
-
+}
 function respostaSelecionada(opcaoSelecionada) {
-   const afirmacoes = opcaoSelecionada.afirmacao;
-   historiaFinal += afirmacoes + " ";
-   atual++;
-   mostraPergunta();
+const afirmacoes = opcaoSelecionada.afirmacao;
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
 }
-
-
 function mostraResultado() {
-   caixaPerguntas.textContent = "Em 2049...";
-   textoResultado.textContent = historiaFinal;
-   caixaAlternativas.textContent = "";
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
 }
-
-
 mostraPergunta();
